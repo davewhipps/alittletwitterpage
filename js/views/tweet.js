4410b56ce81600ec91f37904bbca3174042d4d3d
Dave Whipps <dwhipps@simulationcurriculum.com>
2014-10-30 20:47:36 -0400

    Show date.

--- js/views/tweet.js
+++ js/views/tweet.js
@@ -1,6 +1,6 @@
 ALittleTwitterApp.Views.Tweet = Backbone.View.extend({
 	tagName: 'li',
-	template: _.template('<strong><%= text %></strong>'),
+	template: _.template('<%= text %> </br> <%= created_at %> </br> <%= created_at %>'),
 
   render: function(){
 		this.$el.html(this.template(this.model.toJSON()));
