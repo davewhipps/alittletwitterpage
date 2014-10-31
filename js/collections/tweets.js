5a63995ddd414ddef524f18c9bffb25ba3a597e1
Dave Whipps <dwhipps@simulationcurriculum.com>
2014-10-30 20:17:13 -0400

    Returning tweets properly.

--- js/collections/tweets.js
+++ js/collections/tweets.js
@@ -3,6 +3,6 @@ ALittleTwitterApp.Collections.Tweets = Backbone.Collection.extend({
   url: 'twitter-proxy.php?url='+encodeURIComponent('statuses/user_timeline.json?screen_name=AppDirect&count=30'),
   parse: function(response) {
     console.log('parsing tweets...');
-    return response.results;
+    return response;
   }
 });
